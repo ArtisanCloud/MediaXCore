b5d5af5aecee05d592e30b98ee84966958e092fd
Matrix-X <hbj418@gmail.com>
2025-01-26 00:15:11 +0800

    refact(config): set source and build path

--- pkg/plugin/core/base.go
+++ pkg/plugin/core/base.go
@@ -10,9 +10,10 @@ const (
 
 // 插件描述文件结构
 type PluginMetadata struct {
-	Name    string                 `yaml:"name"`
-	Version string                 `yaml:"version"`
-	Type    PluginType             `yaml:"type"`
-	Path    string                 `yaml:"path"`
-	Config  map[string]interface{} `yaml:"config"`
+	Name       string                 `yaml:"name"`
+	Version    string                 `yaml:"version"`
+	Type       PluginType             `yaml:"type"`
+	SourcePath string                 `yaml:"sourcePath"`
+	BuildPath  string                 `yaml:"buildPath"`
+	Config     map[string]interface{} `yaml:"config"`
 }
