b5f2edbdfaadcee6cc49aa2c62c768b15283ad8f
Matrix-X <hbj418@gmail.com>
2025-02-01 22:33:48 +0800

    refact(contract): compact plugin variable name

--- pkg/plugin/core/contract/provider.go
+++ pkg/plugin/core/contract/provider.go
@@ -1,5 +1,19 @@
 package contract
 
+import "context"
+
+// ---------------------------------------------
+// Provider 定义插件需要实现的接口
+// ---------------------------------------------
+type ProviderInterface interface {
+	// Initialize 插件初始化方法
+	Initialize(ctx *context.Context, config interface{}) error
+	// Name 返回插件名称
+	Name(ctx *context.Context) string
+	// Publish 发布内容的方法
+	Publish(ctx *context.Context, req interface{}) (interface{}, error)
+}
+
 // 定义平台类型（Provider）
 type PluginMediaXProvider string
 
