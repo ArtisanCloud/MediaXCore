2ef3c7c6b6313aef90e4f23427d43cff0c3bcf68
Matrix-X <hbj418@gmail.com>
2025-04-09 10:49:32 +0800

    update

--- pkg/config/provider.go
+++ pkg/config/provider.go
@@ -6,127 +6,127 @@ type VendorApp string
 
 const (
 	// 微信生态（WeChat）平台
-	WechatMediaXVendor MediaXVendor = "WeChat"
+	WechatMediaXVendor MediaXVendor = "we_chat"
 
 	// 微信相关应用
-	WechatOfficialAccount VendorApp = "WechatOfficialAccount" // 公众号
-	WechatMiniProgram     VendorApp = "WechatMiniProgram"     // 小程序
-	WechatChannel         VendorApp = "WechatChannel"         // 视频号
-	WechatWork            VendorApp = "WechatWork"            // 企业微信
+	WechatOfficialAccount VendorApp = "wechat_official_account" // 公众号
+	WechatMiniProgram     VendorApp = "wechat_mini_program"     // 小程序
+	WechatChannel         VendorApp = "wechat_channel"          // 视频号
+	WechatWork            VendorApp = "wechat_work"             // 企业微信
 
 	// 抖音生态（DouYin）平台
-	DouYinMediaXVendor MediaXVendor = "DouYin"
+	DouYinMediaXVendor MediaXVendor = "dou_yin"
 
 	// 抖音相关应用
-	DouYin      VendorApp = "DouYin"      // 抖音
-	DouYinStore VendorApp = "DouYinStore" // 抖音小店
-	DouYinLive  VendorApp = "DouYinLive"  // 抖音直播
-	DouYinAd    VendorApp = "DouYinAd"    // 抖音广告
+	DouYin      VendorApp = "dou_yin"       // 抖音
+	DouYinStore VendorApp = "dou_yin_store" // 抖音小店
+	DouYinLive  VendorApp = "dou_yin_live"  // 抖音直播
+	DouYinAd    VendorApp = "dou_yin_ad"    // 抖音广告
 
 	// 小红书生态（RedBook）平台
-	RedBookMediaXVendor MediaXVendor = "RedBook"
+	RedBookMediaXVendor MediaXVendor = "red_book"
 
 	// 小红书相关应用
-	RedBook      VendorApp = "RedBook"      // 小红书
-	RedBookStore VendorApp = "RedBookStore" // 小红书商城
-	RedBookAd    VendorApp = "RedBookAd"    // 小红书广告
+	RedBook      VendorApp = "red_book"       // 小红书
+	RedBookStore VendorApp = "red_book_store" // 小红书商城
+	RedBookAd    VendorApp = "red_book_ad"    // 小红书广告
 
 	// 快手生态（Kuaishou）平台
-	KuaishouMediaXVendor MediaXVendor = "Kuaishou"
+	KuaishouMediaXVendor MediaXVendor = "kuaishou"
 
 	// 快手相关应用
-	Kuaishou      VendorApp = "Kuaishou"      // 快手
-	KuaishouLive  VendorApp = "KuaishouLive"  // 快手直播
-	KuaishouStore VendorApp = "KuaishouStore" // 快手小店
-	KuaishouAd    VendorApp = "KuaishouAd"    // 快手广告
+	Kuaishou      VendorApp = "kuaishou"       // 快手
+	KuaishouLive  VendorApp = "kuaishou_live"  // 快手直播
+	KuaishouStore VendorApp = "kuaishou_store" // 快手小店
+	KuaishouAd    VendorApp = "kuaishou_ad"    // 快手广告
 
 	// Bilibili 生态（Bilibili）平台
-	BilibiliMediaXVendor MediaXVendor = "Bilibili"
+	BilibiliMediaXVendor MediaXVendor = "bilibili"
 
 	// Bilibili 相关应用
-	Bilibili     VendorApp = "Bilibili"     // Bilibili
-	BilibiliLive VendorApp = "BilibiliLive" // Bilibili 直播
-	BilibiliAd   VendorApp = "BilibiliAd"   // Bilibili 广告
+	Bilibili     VendorApp = "bilibili"      // Bilibili
+	BilibiliLive VendorApp = "bilibili_live" // Bilibili 直播
+	BilibiliAd   VendorApp = "bilibili_ad"   // Bilibili 广告
 
 	// 知乎生态（Zhihu）平台
-	ZhihuMediaXVendor MediaXVendor = "Zhihu"
+	ZhihuMediaXVendor MediaXVendor = "zhihu"
 
 	// 知乎相关应用
-	Zhihu     VendorApp = "Zhihu"     // 知乎
-	ZhihuAd   VendorApp = "ZhihuAd"   // 知乎广告
-	ZhihuLive VendorApp = "ZhihuLive" // 知乎 Live
+	Zhihu     VendorApp = "zhihu"      // 知乎
+	ZhihuAd   VendorApp = "zhihu_ad"   // 知乎广告
+	ZhihuLive VendorApp = "zhihu_live" // 知乎 Live
 
 	// 微博生态（Weibo）平台
-	WeiboMediaXVendor MediaXVendor = "Weibo"
+	WeiboMediaXVendor MediaXVendor = "weibo"
 
 	// 微博相关应用
-	Weibo   VendorApp = "Weibo"   // 微博
-	WeiboAd VendorApp = "WeiboAd" // 微博广告
+	Weibo   VendorApp = "weibo"    // 微博
+	WeiboAd VendorApp = "weibo_ad" // 微博广告
 
 	// YouTube 平台
-	GoogleMediaXVendor MediaXVendor = "Google"
+	GoogleMediaXVendor MediaXVendor = "google"
 
 	// YouTube 相关应用
-	YouTube       VendorApp = "YouTube"       // YouTube
-	YouTubeAd     VendorApp = "YouTubeAd"     // YouTube 广告
-	YouTubeShorts VendorApp = "YouTubeShorts" // YouTube Shorts
-	Blogger       VendorApp = "Blogger"       // YouTube Shorts
+	YouTube       VendorApp = "youtube"        // YouTube
+	YouTubeAd     VendorApp = "youtube_ad"     // YouTube 广告
+	YouTubeShorts VendorApp = "youtube_shorts" // YouTube Shorts
+	Blogger       VendorApp = "blogger"        // 博客平台
 
 	// TikTok 平台
-	TikTokMediaXVendor MediaXVendor = "TikTok"
+	TikTokMediaXVendor MediaXVendor = "tiktok"
 
 	// TikTok 相关应用
-	TikTok     VendorApp = "TikTok"     // TikTok
-	TikTokAd   VendorApp = "TikTokAd"   // TikTok 广告
-	TikTokShop VendorApp = "TikTokShop" // TikTok 小店
-	TikTokLive VendorApp = "TikTokLive" // TikTok 直播
+	TikTok     VendorApp = "tiktok"      // TikTok
+	TikTokAd   VendorApp = "tiktok_ad"   // TikTok 广告
+	TikTokShop VendorApp = "tiktok_shop" // TikTok 小店
+	TikTokLive VendorApp = "tiktok_live" // TikTok 直播
 
 	// Facebook 平台
-	FacebookMediaXVendor MediaXVendor = "Facebook"
+	FacebookMediaXVendor MediaXVendor = "facebook"
 
 	// Facebook 相关应用
-	Facebook      VendorApp = "Facebook"      // Facebook
-	FacebookAd    VendorApp = "FacebookAd"    // Facebook 广告
-	FacebookLive  VendorApp = "FacebookLive"  // Facebook 直播
-	FacebookReels VendorApp = "FacebookReels" // Facebook Reels 短视频
+	Facebook      VendorApp = "facebook"       // Facebook
+	FacebookAd    VendorApp = "facebook_ad"    // Facebook 广告
+	FacebookLive  VendorApp = "facebook_live"  // Facebook 直播
+	FacebookReels VendorApp = "facebook_reels" // Facebook Reels 短视频
 
 	// Instagram 平台
-	InstagramMediaXVendor MediaXVendor = "Instagram"
+	InstagramMediaXVendor MediaXVendor = "instagram"
 
 	// Instagram 相关应用
-	Instagram      VendorApp = "Instagram"      // Instagram
-	InstagramAd    VendorApp = "InstagramAd"    // Instagram 广告
-	InstagramReels VendorApp = "InstagramReels" // Instagram Reels
+	Instagram      VendorApp = "instagram"       // Instagram
+	InstagramAd    VendorApp = "instagram_ad"    // Instagram 广告
+	InstagramReels VendorApp = "instagram_reels" // Instagram Reels
 
 	// Twitter（X）平台
-	TwitterMediaXVendor MediaXVendor = "Twitter"
+	TwitterMediaXVendor MediaXVendor = "twitter"
 
 	// Twitter 相关应用
-	Twitter       VendorApp = "Twitter"       // Twitter
-	TwitterAd     VendorApp = "TwitterAd"     // Twitter 广告
-	TwitterSpaces VendorApp = "TwitterSpaces" // Twitter Spaces（音频直播）
+	Twitter       VendorApp = "twitter"        // Twitter
+	TwitterAd     VendorApp = "twitter_ad"     // Twitter 广告
+	TwitterSpaces VendorApp = "twitter_spaces" // Twitter Spaces（音频直播）
 
 	// LinkedIn 平台
-	LinkedInMediaXVendor MediaXVendor = "LinkedIn"
+	LinkedInMediaXVendor MediaXVendor = "linked_in"
 
 	// LinkedIn 相关应用
-	LinkedIn     VendorApp = "LinkedIn"     // LinkedIn
-	LinkedInAd   VendorApp = "LinkedInAd"   // LinkedIn 广告
-	LinkedInLive VendorApp = "LinkedInLive" // LinkedIn 直播
+	LinkedIn     VendorApp = "linked_in"      // LinkedIn
+	LinkedInAd   VendorApp = "linked_in_ad"   // LinkedIn 广告
+	LinkedInLive VendorApp = "linked_in_live" // LinkedIn 直播
 
 	// Twitch 平台
-	TwitchMediaXVendor MediaXVendor = "Twitch"
+	TwitchMediaXVendor MediaXVendor = "twitch"
 
 	// Twitch 相关应用
-	Twitch     VendorApp = "Twitch"     // Twitch
-	TwitchAd   VendorApp = "TwitchAd"   // Twitch 广告
-	TwitchLive VendorApp = "TwitchLive" // Twitch 直播
+	Twitch     VendorApp = "twitch"      // Twitch
+	TwitchAd   VendorApp = "twitch_ad"   // Twitch 广告
+	TwitchLive VendorApp = "twitch_live" // Twitch 直播
 
 	// Snapchat 平台
-	SnapchatMediaXVendor MediaXVendor = "Snapchat"
+	SnapchatMediaXVendor MediaXVendor = "snapchat"
 
 	// Snapchat 相关应用
-	Snapchat          VendorApp = "Snapchat"          // Snapchat
-	SnapchatAd        VendorApp = "SnapchatAd"        // Snapchat 广告
-	SnapchatSpotlight VendorApp = "SnapchatSpotlight" // Snapchat Spotlight 短视频
+	Snapchat          VendorApp = "snapchat"           // Snapchat
+	SnapchatAd        VendorApp = "snapchat_ad"        // Snapchat 广告
+	SnapchatSpotlight VendorApp = "snapchat_spotlight" // Snapchat Spotlight 短视频
 )
